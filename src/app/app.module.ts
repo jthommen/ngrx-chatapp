818eb7800c29c9599184a9e301e4199dc3d13516
Juri Thommen <jthommen@google.com>
2017-12-07 17:32:07 +0000

    Scaffold angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import * as _ from 'lodash';
+import { UserSelectionComponent } from './user-selection/user-selection.component';
+import { ThreadSectionComponent } from './thread-section/thread-section.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
+import { MessageListComponent } from './message-list/message-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSelectionComponent,
+    ThreadSectionComponent,
+    MessageSectionComponent,
+    ThreadListComponent,
+    MessageListComponent
   ],
   imports: [
     BrowserModule
